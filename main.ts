2b77f8aa189b1543ea0e1f5420f9f18ffb506b27
Chauvelf <128042162+Chauvelf@users.noreply.github.com>
2023-03-16 16:44:07 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,38 +1,32 @@
 input.onSound(DetectedSound.Loud, function () {
     Stop.showColor(neopixel.colors(NeoPixelColors.Red))
-    Stop.show()
     basic.pause(2000)
-    Gauche.showColor(neopixel.colors(NeoPixelColors.Black))
+    Stop.showColor(neopixel.colors(NeoPixelColors.Black))
 })
 input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 4; index++) {
         Gauche.showColor(neopixel.colors(NeoPixelColors.Orange))
-        Gauche.show()
-        basic.pause(50)
+        basic.pause(500)
         Gauche.showColor(neopixel.colors(NeoPixelColors.Black))
-        basic.pause(50)
+        basic.pause(500)
     }
-})
-input.onSound(DetectedSound.Quiet, function () {
-    Stop.showColor(neopixel.colors(NeoPixelColors.Black))
-    Stop.show()
 })
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < 4; index++) {
         Droite.showColor(neopixel.colors(NeoPixelColors.Orange))
         Droite.show()
-        basic.pause(50)
-        Gauche.showColor(neopixel.colors(NeoPixelColors.Black))
-        basic.pause(50)
+        basic.pause(500)
+        Droite.showColor(neopixel.colors(NeoPixelColors.Black))
+        basic.pause(500)
     }
 })
-let Stop: neopixel.Strip = null
 let Droite: neopixel.Strip = null
+let Stop: neopixel.Strip = null
 let Gauche: neopixel.Strip = null
 let Strip = neopixel.create(DigitalPin.P16, 5, NeoPixelMode.RGB)
-Gauche = Strip.range(1, 2)
-Droite = Strip.range(3, 4)
+Gauche = Strip.range(0, 2)
 Stop = Strip.range(0, 5)
+Droite = Strip.range(3, 2)
 basic.forever(function () {
 	
 })
